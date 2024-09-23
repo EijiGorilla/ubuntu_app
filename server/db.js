1616a79d3bfe77414a6a4ecb395f7f2603bd3e09
EijiGorilla <emasuz@hotmail.com>
2024-09-24 07:20:48 +0800

    updated

--- server/db.js
+++ server/db.js
@@ -1,9 +1,13 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "postgres",
-  password: "eijipostgre",
-  host: "localhost",
-  port: 5432,
-  database: "test",
-});
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "eijipostgre",
+//   host: "localhost",
+//   port: 5432,
+//   database: "test",
+// });
+// module.exports = pool;
+
+const Pool = require("pg");
+const pool = new Pool();
 module.exports = pool;
