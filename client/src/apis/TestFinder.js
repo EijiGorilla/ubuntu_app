bad2011af33fcf0323dc4c52a9bb8b1b04065796
EijiGorilla <emasuz@hotmail.com>
2024-09-27 10:10:45 +0800

    updated

--- client/src/apis/TestFinder.js
+++ client/src/apis/TestFinder.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 // const baseURL = process.env.NODE_ENV === 'production' ? '/*' : 'http://localhost:5000/*';
 
-const baseURL = 'http://13.236.193.199:5000';
+const baseURL =
+  process.env.NODE_ENV === 'production' ? 'http://13.236.193.199:5000' : 'http://localhost:5000/*';
 
 export default axios.create({
   baseURL,
